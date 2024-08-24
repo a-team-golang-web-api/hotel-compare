2a8b419a42e30cfa038b4a0cf6508903dfaec28f
yziori <ms6rx7861@gmail.com>
2024-08-24 19:05:51 +0900

    feat: ヘッダーUI実装

---
+++ frontend/app/_components/layouts/Header/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+/**
+ * ヘッダーコンポーネント
+ */
+const Header = () => {
+	return (
+		<div className="navbar bg-base-100">
+			<div className="btn btn-ghost text-xl">
+				<Image
+					src="recursion-logo-square.png"
+					width={32}
+					height={32}
+					alt={""}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
