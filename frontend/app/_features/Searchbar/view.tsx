c821c970189d4dc0df050c7d57419bb8c9412c2c
yziori <ms6rx7861@gmail.com>
2024-08-25 22:47:33 +0900

    💄 style: 検索バーコンポーネントに背景色と枠を追加

--- frontend/app/_features/Searchbar/view.tsx
+++ frontend/app/_features/Searchbar/view.tsx
@@ -23,7 +23,7 @@ export const SearchbarView = () => {
 	const [checkOutDate, setCheckOutDate] = useState("");
 
 	return (
-		<div className="container mx-auto p-4 space-y-4">
+		<div className="container mx-auto p-4 space-y-4 bg-white shadow-lg rounded-lg">
 			<div className="container flex items-end space-x-3">
 				{/* TODO: destinationOptionは仮置き。apiから取得する */}
 				<Selectbox labelText="目的地" options={destinationOption} />
