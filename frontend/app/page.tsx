163e7b67f22d5280ee869bd633854cfe7946d310
yziori <ms6rx7861@gmail.com>
2024-08-25 22:53:22 +0900

    💄 style: 検索バーのレイアウト調整

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,9 @@ import Searchbar from "./_features/Searchbar";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between ">
-			<Searchbar />
+			<div className="w-full max-w-4xl mx-auto my-4">
+				<Searchbar />
+			</div>
 			<HotelList />
 		</main>
 	);
